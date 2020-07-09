df1f168689dec41565274331379a631b4b64dc63
Administrator <admin@example.com>
2020-07-09 06:55:21 +0000

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import TableHead from './TableHead';
-import TableFoot from './TableFoot';
-import TableBody from './TableBody';
 
 function Table() {
   return (
-    <table>
-      <TableHead></TableHead>
-      <TableBody></TableBody>
-      <TableFoot></TableFoot>
-    </table>
   )
 }
 
